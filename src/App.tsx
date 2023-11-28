e478161a6635fbef165da5deb1584d2af2742885
jjitch <junnosuke_08@icloud.com>
2023-11-29 01:18:44 +0900

    add navi to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { useState } from "react";
 import XmlForm from "./XmlForm";
 import XmlDocView from "./XmlDocView";
 import "./App.css";
+import { XPathNavigator } from "./XPathNavigator";
 
 function App() {
     const [xmlContent, setXmlcontent] = useState<string>("");
 
     return (
-        <div>
+        <>
             <XmlForm onFileUploaded={setXmlcontent} />
+            <XPathNavigator />
             <XmlDocView content={xmlContent} />
-        </div>
+        </>
     );
 }
 
